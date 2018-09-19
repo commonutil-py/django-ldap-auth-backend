20b7d9802efefa1a130ee7072d55fdec310e079b
Cheyin L <cheyinl@gmail.com>
2018-09-19 14:34:13 +0800

    adjust field name to avoid name clash

--- ldapauthbackend/models.py
+++ ldapauthbackend/models.py
@@ -8,8 +8,8 @@ from django.db import models
 class LDAPUserProfile(models.Model):
 	user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
 	dn = models.CharField(max_length=255)
-	user_id = models.BigIntegerField(default=-1)
-	user_name = models.CharField(max_length=127)
+	account_uid = models.BigIntegerField(default=-1)
+	account_name = models.CharField(max_length=127)
 	import_staff = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True, related_name="+", editable=False)
 
 	def __unicode__(self):
