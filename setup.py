adf2f9d86ec6ae35fdbb0069deefc07ad040ecf5
Cheyin L <cheyinl@gmail.com>
2022-03-07 14:07:53 +0800

    bump revision

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup(
 		name="django-ldap-auth-backend",
-		version="0.1.3",  # REV-CONSTANT:rev 5d022db7d38f580a850cd995e26a6c2f
+		version="0.3.1",  # REV-CONSTANT:rev 5d022db7d38f580a850cd995e26a6c2f
 		description="LDAP authentication backend for Django",
 		packages=[
 				"ldapauthbackend",
@@ -13,15 +13,15 @@ setup(
 		],
 		include_package_data=True,
 		install_requires=[
-				"python-ldap ~= 3.4.0",
-				"django-angular-host-page-template-backend ~= 0.1.0",
+				"python-ldap >= 3.4.0, < 4.0.0",
+				"django-angular-host-page-template-backend >= 0.3.0, < 0.4.0",
 		],
 		classifiers=[
 				'Development Status :: 5 - Production/Stable',
 				'Intended Audience :: Developers',
 				'License :: OSI Approved :: MIT License',
 				'Programming Language :: Python :: 3.7',
-				'Framework :: Django :: 1.11',
+				'Framework :: Django :: 3.2',
 		],
 		license='MIT License',
 )
